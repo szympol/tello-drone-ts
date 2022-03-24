6da6d1ef615ef89ee3b572d89d729ee3cc946a23
Szymon Polanowski <szymon.polanowski@bitpanda.com>
2022-03-24 19:01:43 +0100

    Add git hooks

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,19 @@ interface IUser {
   surname: string;
 }
 
-const users: Array<IUser> = [{
-  name: 'Nela',
-  surname: 'Polanowska',
-}, {
-  name: 'Iwona',
-  surname: 'Polanowska',
-}, {
-
-  name: 'Szymon',
-  surname: 'Polanowski',
-},
+const users: Array<IUser> = [
+  {
+    name: 'Nela',
+    surname: 'Polanowska',
+  },
+  {
+    name: 'Iwona',
+    surname: 'Polanowska',
+  },
+  {
+    name: 'Szymon',
+    surname: 'Polanowski',
+  },
 ];
 
 const getUser = (name: IUser['name']): IUser | undefined => users.find((user) => user.name === name);
