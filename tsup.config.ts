f092c966c1064703cc441b9e3d60f661d861eb99
Szymon Polanowski <szymon.polanowski@docfinder.at>
2025-01-01 17:55:39 +0100

    chore: drop commonjs build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "build",
-  format: ["esm", "cjs"],
+  format: ["esm"],
   sourcemap: true,
   bundle: true,
   splitting: false,
